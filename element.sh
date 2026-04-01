44a509e6f623e461355c83b60e4d2e798c7da92e
mazenn196-sketch <mazenn196@gmail.com>
2026-04-01 07:50:37 -0400

    chore: final cleanup

--- element.sh
+++ element.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
-  exit
-fi
-
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align --field-separator='|' -c"
-
-RESULT=$($PSQL "
-  SELECT e.atomic_number, e.symbol, e.name, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type
-  FROM elements e
-  JOIN properties p ON e.atomic_number = p.atomic_number
-  JOIN types t ON p.type_id = t.type_id
-  WHERE e.atomic_number::TEXT = '$1'
-     OR e.symbol = '$1'
-     OR e.name = '$1';
-")
-
-if [[ -z $RESULT ]]
-then
-  echo "I could not find that element in the database."
 else
-  echo "$RESULT" | while IFS='|' read -r ATOMIC_NUMBER SYMBOL NAME MASS MELT BOIL TYPE
-  do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
-  done
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    CONDITION="e.atomic_number = $1"
+  else
+    CONDITION="e.symbol = '$1' OR e.name = '$1'"
+  fi
+
+  RESULT=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE $CONDITION;")
+
+  if [[ -z $RESULT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo "$RESULT" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME MASS MELT BOIL TYPE
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+    done
+  fi
 fi
