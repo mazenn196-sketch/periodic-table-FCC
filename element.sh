99541eb45eeafda97e52b7aafab1fb0fd963cf36
mazenn196-sketch <mazenn196@gmail.com>
2026-04-01 07:34:19 -0400

    fix: update element.sh for exact output formatting

---
+++ element.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align --field-separator='|' -c"
