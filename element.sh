c9842a5a2457b19f917aa8f9ab47dc2ed86bf54e
mazenn196-sketch <mazenn196@gmail.com>
2026-04-01 07:35:28 -0400

    refactor: improve database query

--- element.sh
+++ element.sh
@@ -7,3 +7,13 @@ then
 fi
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align --field-separator='|' -c"
+
+RESULT=$($PSQL "
+  SELECT e.atomic_number, e.symbol, e.name, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type
+  FROM elements e
+  JOIN properties p ON e.atomic_number = p.atomic_number
+  JOIN types t ON p.type_id = t.type_id
+  WHERE e.atomic_number::TEXT = '$1'
+     OR e.symbol = '$1'
+     OR e.name = '$1';
+")
