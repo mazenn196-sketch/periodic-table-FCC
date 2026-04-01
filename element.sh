8b4e3cf2e2d3b493ed4c6d3907e251ffe812ae76
mazenn196-sketch <mazenn196@gmail.com>
2026-04-01 07:36:25 -0400

    test: verify output for all cases

--- element.sh
+++ element.sh
@@ -17,3 +17,13 @@ RESULT=$($PSQL "
      OR e.symbol = '$1'
      OR e.name = '$1';
 ")
+
+if [[ -z $RESULT ]]
+then
+  echo "I could not find that element in the database."
+else
+  echo "$RESULT" | while IFS='|' read -r ATOMIC_NUMBER SYMBOL NAME MASS MELT BOIL TYPE
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+  done
+fi
